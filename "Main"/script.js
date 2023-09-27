b10d1d7dc6213e8e310f0c0694254cf5f645da5d
simsjh <simonsjoman@gmail.com>
2023-09-27 11:51:33 +0200

    Nyaste versionen av Main, uppdaterar portrtät

--- "Main"/script.js
+++ "Main"/script.js
@@ -138,6 +138,24 @@ const kundvagnApp = Vue.createApp({
 // Tillåter Vue att genom mount förändra HTML-element med ID 'kundvagnApp'.
 kundvagnApp.mount('#kundvagnApp');
 
+const popup = () => {
+    const popups = document.querySelectorAll(".popup");
+    
+    for (let i=0; i<popups.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = popups[i].getBoundingClientRect().top;
+        const elementVisible = 150; 
+        
+        if (elementTop < windowHeight - elementVisible) {
+            popups[i].classList.add("active");
+        } 
+        else { 
+            popups[i].classList.remove("active");
+        } 
+    } 
+}
+window.addEventListener("scroll", popup); 
+
 // JavaScript kode för bildspelsnavigation
 //Skapar och sparar bilderna här
 const images = document.querySelectorAll('.gallery-image');
